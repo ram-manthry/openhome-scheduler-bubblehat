42f7e1132eb3603f20aa904313038a95dcf1ba1f
James <hoh020@aucklanduni.ac.nz>
2014-09-20 10:52:04 +1200

    Added open home watch list
    Changed the default ‘/‘ location to only list open homes from watch
    list. Created a new page ‘/watchlist’ to show ALL watch lists.

---
+++ middlewares/openhome.js
@@ -0,0 +1,24 @@
+var request = require('request');
+
+// Middleware to retrieve the user home timeline
+// Show only open homes in your watchlist.
+exports.watchlist = function(req, res, next) {
+	req.trademe = req.trademe || {};
+
+	var oauth = {
+		consumer_key: req.app.set('oauth consumer key'),
+		consumer_secret: req.app.set('oauth consumer secret'),
+		token: req.session.oauth.access_token,
+		token_secret: req.session.oauth.access_token_secret
+	},
+url = 'https://api.' + req.app.set('api domain') + '/v1/MyTradeMe/Watchlist/OpenHomes.json';
+
+	request.get({
+		url: url,
+		oauth: oauth,
+		json: true
+	}, function(e, r, result) {
+		req.trademe.watchlist = result;
+		next();
+	});
+};
