1317e9f5db524a68a474bef650fe51b6277b4a26
James <hoh020@aucklanduni.ac.nz>
2014-09-20 16:01:22 +1200

    visit time
    visit time

--- models/visitTime.js
+++ models/visitTime.js
@@ -2,8 +2,8 @@
 var moment = require('moment');
 var VisitTime = function (startTime, endTime) {
 	var k = {};
-	k.startTime = new Date(moment(startTime).format('MMM DD YYYY'));
-	k.endTime = new Date(moment(endTime).format('MMM DD YYYY'));
+	k.startTime = new Date(moment(startTime));
+	k.endTime = new Date(moment(endTime));
 
 
 	return k;
